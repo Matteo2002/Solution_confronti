c6722d10c00704be5699aded5c5432d9cf4f903a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 09:13:24 +0100

    Creazione di due test per il metodo che trova il massimo tra due numeri

--- Confronto_library/Confronto.cs
+++ Confronto_library/Confronto.cs
@@ -4,6 +4,22 @@ namespace Confronto_library
 {
     public static class Confronto
     {
+        public static int Max(int n1, int n2)
+        {
+            int max = 0;
+
+            if (n1 > n2)
+            {
+                max = n1;
+            }
+            else
+            {
+                max = n2;
+            }
+
+            return max;
+        }
+
 
     }
 }
--- Confronto_library_tests/Confronto_tests.cs
+++ Confronto_library_tests/Confronto_tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Confronto_library;
 
 namespace Confronto_library_tests
 {
@@ -9,7 +10,25 @@ namespace Confronto_library_tests
         [TestMethod]
         public void TestMax()
         {
+            int n1 = 0;
+            int n2 = 7;
+            int max_aspettato = 7;
 
+            int max_calcolato = Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspettato, max_calcolato);
+
+        }
+        [TestMethod]
+        public void TestMax1()
+        {
+            int n1 = 5;
+            int n2 = 3;
+            int max_aspettato = 5;
+
+            int max_calcolato = Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspettato, max_calcolato);
+
+          
         }
     }
 }
